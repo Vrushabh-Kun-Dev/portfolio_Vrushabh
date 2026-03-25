ca7e7646899bea2687ccf122d196d4cd2d1c0bff
Vrushabh-Kun-Dev <vrushabh.kun.dev@gmail.com>
2026-03-25 14:49:12 +0530

    Navbar added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <div className="App">
+        <Navbar/>
+      </div>
+    </>
+  )
+}
+
+
+export default App;
