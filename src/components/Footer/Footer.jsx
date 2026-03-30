65e2973a054eb0983e8d3b809b2073c8f7ded973
Vrushabh-Kun-Dev <vrushabh.kun.dev@gmail.com>
2026-03-30 13:33:12 +0530

    contact and footer section added

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+const Footer = () => {
+    return (
+        <>
+            <p className="footer">Copyright &#169; 2026 Vrushabh Kunturwar. All Rights Reserved</p>
+        </>
+    )
+}
+
+export default Footer;
